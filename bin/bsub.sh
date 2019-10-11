a8b336f42dea1e8b3521465d4843ed019d84cb31
gn5 <gn5@sanger.ac.uk>
2019-10-11 15:11:56 +0100

    auto-git

--- bin/bsub.sh
+++ bin/bsub.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
+export LSB_DEFAULTGROUP=hgi
+export CAPSULE_CACHE_DIR=/lustre/scratch115/projects/interval_wgs/nextflow/cache
 export GOPATH=/lustre/scratch115/projects/interval_wgs/nextflow/wr_latest/wr/gopath
 export PATH=/lustre/scratch115/projects/interval_wgs/nextflow/wr0.19.9:$PATH
-export LSB_DEFAULTGROUP=hgi
-export CAPSULE_CACHE_DIR=/software/hgi/envs/
-export CONDA_ENVS_DIRS=/software/hgi/envs/
-export CONDA_PKGS_DIRS=/software/hgi/envs/
-# conda activate /software/hgi/envs
+export CONDA_ENVS_DIRS=/lustre/scratch115/realdata/mdt2/projects/bioaid/mercury_gn5/bioaid/.conda
+export CONDA_PKGS_DIRS=/lustre/scratch115/realdata/mdt2/projects/bioaid/mercury_gn5/bioaid/.conda
 
 echo starting bsub
 rm -f hs_err_pid* && rm -f timeline* && rm -f trace* && rm -rf report* && rm -f bsub.o && rm -f bsub.e && rm -f .nextflow.log && bsub -G hgi -R'select[mem>8000] rusage[mem=8000] span[hosts=1]' -M 8000 -n 2 -o bsub.o -e bsub.e -q yesterday ./bin/nextflow.sh > bjob.id
