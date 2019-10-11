83e679aca9f62b2c702ae4b3801849e14e1ffec3
gn5 <gn5@sanger.ac.uk>
2019-10-11 11:29:43 +0100

    auto-git

---
+++ bin/nohup_local_farm.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+export LSB_DEFAULTGROUP=hgi
+export CAPSULE_CACHE_DIR=/lustre/scratch115/projects/interval_wgs/nextflow/cache
+export GOPATH=/lustre/scratch115/projects/interval_wgs/nextflow/wr_latest/wr/gopath
+export PATH=/lustre/scratch115/projects/interval_wgs/nextflow/wr0.19.9:$PATH
+export CONDA_ENVS_DIRS=/lustre/scratch115/realdata/mdt2/projects/bioaid/mercury_gn5/bioaid/.conda
+export CONDA_PKGS_DIRS=/lustre/scratch115/realdata/mdt2/projects/bioaid/mercury_gn5/bioaid/.conda
+rm -f hs_err_pid* && rm -f timeline* && rm -f trace* && rm -rf report* && rm -f bsub.o && rm -f bsub.e && rm -f .nextflow.log && rm -f nohup.log && rm -f nohup.out && nohup ./bin/nextflow.sh > nohup.log 2>&1 &
