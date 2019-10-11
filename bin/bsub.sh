d2f6ab0200b7a9d10ed9f1bc0d82c5e3556ae831
gn5 <gn5@sanger.ac.uk>
2019-10-11 15:08:36 +0100

    auto-git

--- bin/bsub.sh
+++ bin/bsub.sh
@@ -8,6 +8,6 @@ export CONDA_PKGS_DIRS=/software/hgi/envs/
 # conda activate /software/hgi/envs
 
 echo starting bsub
-rm -f hs_err_pid* && rm -f timeline* && rm -f trace* && rm -rf report* && rm -f bsub.o && rm -f bsub.e && rm -f .nextflow.log && bsub -G hgi -R'select[mem>8000] rusage[mem=8000] span[hosts=1]' -M 8000 -n 2 -o bsub.o -e bsub.e -q basement ./bin/nextflow.sh > bjob.id
+rm -f hs_err_pid* && rm -f timeline* && rm -f trace* && rm -rf report* && rm -f bsub.o && rm -f bsub.e && rm -f .nextflow.log && bsub -G hgi -R'select[mem>8000] rusage[mem=8000] span[hosts=1]' -M 8000 -n 2 -o bsub.o -e bsub.e -q yesterday ./bin/nextflow.sh > bjob.id
 echo finished bsub
 cat bjob.id
