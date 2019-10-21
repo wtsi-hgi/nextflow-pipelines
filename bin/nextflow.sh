b7241a29b29681aa83e14da550732ee8a58f2b78
gn5 <gn5@sanger.ac.uk>
2019-10-21 14:59:41 +0100

    auto-git

--- bin/nextflow.sh
+++ bin/nextflow.sh
@@ -6,5 +6,5 @@ export NXF_OPTS="-Xms6G -Xmx6G -Dnxf.pool.type=sync -Dnxf.pool.maxThreads=2000"
 export NXF_JAVA_HOME="/lustre/scratch115/realdata/mdt2/projects/bioaid/mercury_gn5/bioaid/env"
 export JAVA_HOME="/lustre/scratch115/realdata/mdt2/projects/bioaid/mercury_gn5/bioaid/env"
 export JAVA_CMD="/lustre/scratch115/realdata/mdt2/projects/bioaid/mercury_gn5/bioaid/env/bin/java"
-nextflow run pipelines/crispr.nf -profile farm4_singularity_gn5 -resume 
+nextflow run ./pipelines/crispr.nf -profile farm4_singularity_gn5 -resume 
 #nextflow run main.nf -profile farm4_singularity_gn5 -resume 
