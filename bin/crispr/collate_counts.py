061f037ed1d94b67e2bfa066e103f8119362fe9b
gn5 <gn5@sanger.ac.uk>
2019-10-22 10:08:42 +0100

    auto-git

--- bin/crispr/collate_counts.py
+++ bin/crispr/collate_counts.py
@@ -13,6 +13,7 @@ for index, row in fofn_countsfiles.iterrows():
         to_concatenate = pd.read_csv(row['count_file'])
         to_concatenate['samplename']=row['samplename']
         concatenated = pd.concat([concatenated, to_concatenate], ignore_index=True)
+    print(concatenated.head())
         
 count_matrix = concatenated.pivot(index='Guide Sequence', columns='samplename', values='Count')       
 count_matrix.to_csv(lib_counts + '.count_matrix.txt', sep='\t')
