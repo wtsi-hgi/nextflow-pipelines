7b3c142e62525175b98fe9ef25240e46406666ce
gn5 <gn5@sanger.ac.uk>
2019-10-25 12:19:56 +0100

    auto-git

--- bin/nohup_local_farm.sh
+++ bin/nohup_local_farm.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 export LSB_DEFAULTGROUP=hgi
-export CAPSULE_CACHE_DIR=/lustre/scratch115/projects/interval_wgs/nextflow/cache
 export GOPATH=/lustre/scratch115/projects/interval_wgs/nextflow/wr_latest/wr/gopath
 export PATH=/lustre/scratch115/projects/interval_wgs/nextflow/wr0.19.9:$PATH
-export CONDA_ENVS_DIRS=/lustre/scratch115/realdata/mdt2/projects/bioaid/mercury_gn5/bioaid/.conda
-export CONDA_PKGS_DIRS=/lustre/scratch115/realdata/mdt2/projects/bioaid/mercury_gn5/bioaid/.conda
+export CAPSULE_CACHE_DIR=/software/hgi/installs/conda/capsule_cache
+export CONDA_ENVS_DIRS=/software/hgi/installs/conda/conda_envs
+export CONDA_PKGS_DIRS=/software/hgi/installs/conda/conda_dirs
+export PATH=/software/hgi/installs/conda/conda_envs/nextflow/bin:$PATH
 export PATH=/software/singularity-v3.2.0/bin:$PATH
+# conda activate $CONDA_ENVS_DIRS/nextflow
+
 rm -f hs_err_pid* && rm -f timeline* && rm -f trace* && rm -rf report* && rm -f bsub.o && rm -f bsub.e && rm -f .nextflow.log && rm -f nohup.log && rm -f nohup.out && nohup ./bin/nextflow.sh > nohup.log 2>&1 &
