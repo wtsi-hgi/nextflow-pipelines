bd11331723c3e6bdf2263e1d03f2b10685f234b7
gn5 <gn5@sanger.ac.uk>
2019-10-30 11:08:46 +0000

    auto-git

--- bin/nextflow.sh
+++ bin/nextflow.sh
@@ -2,9 +2,10 @@
 echo starting nextflow
 
 # export NXF_VER=19.09.0-edge
-export NXF_OPTS="-Xms8G -Xmx8G -Dnxf.pool.type=sync -Dnxf.pool.maxThreads=2000"
+#export NXF_OPTS="-Xms8G -Xmx8G -Dnxf.pool.type=sync -Dnxf.pool.maxThreads=2000"
+export NXF_OPTS="-Xms8G -Xmx8G -Dnxf.pool.maxThreads=2000"
 # export NXF_JAVA_HOME="/lustre/scratch115/realdata/mdt2/projects/bioaid/mercury_gn5/bioaid/env"
 # export JAVA_HOME="/lustre/scratch115/realdata/mdt2/projects/bioaid/mercury_gn5/bioaid/env"
 # export JAVA_CMD="/lustre/scratch115/realdata/mdt2/projects/bioaid/mercury_gn5/bioaid/env/bin/java"
 nextflow run ./pipelines/crispr.nf -profile farm4_singularity_gn5 -resume 
-#nextflow run main.nf -profile farm4_singularity_gn5 -resume 
+# nextflow run main.nf -profile farm4_singularity_gn5 -resume 
