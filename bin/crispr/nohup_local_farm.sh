794092dea2ae5d59db5d6ea45206ccf8bfa35c81
gn5 <gn5@sanger.ac.uk>
2019-11-01 15:39:58 +0000

    auto-git

--- bin/crispr/nohup_local_farm.sh
+++ bin/crispr/nohup_local_farm.sh
@@ -21,6 +21,7 @@ export CONDA_ENVS_DIRS=/software/hgi/installs/anaconda3/envs
 export CONDA_PKGS_DIRS=/software/hgi/installs/anaconda3/pkgs
 #export CAPSULE_CACHE_DIR=/lustre/scratch114/projects/gains_team282/Nextflow-RNAseq-gains/capsules
 export CAPSULE_CACHE_DIR=/software/hgi/installs/anaconda3/capsule_cache
+eval "$(conda shell.bash hook)"
 conda activate $CONDA_ENVS_DIRS/nextflow
 
 rm -f hs_err_pid* && rm -f timeline* && rm -f trace* && rm -rf report* && rm -f bsub.o && rm -f bsub.e && rm -f .nextflow.log && rm -f nohup.log && rm -f nohup.out && nohup ./nextflow-pipelines/bin/crispr/nextflow.sh > nohup.log 2>&1 &
