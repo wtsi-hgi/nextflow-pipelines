5f23b4996cd696428cf9156a64c2491f6c96070c
gn5 <gn5@sanger.ac.uk>
2019-11-01 11:40:59 +0000

    auto-git

--- bin/irods_fetch.nf/nextflow.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-echo starting nextflow
-
-export NXF_OPTS="-Xms8G -Xmx8G -Dnxf.pool.maxThreads=2000"
-nextflow run ./pipelines/irods_fetch.nf -profile farm4_singularity_gn5 --samplefile ./data/single_file_samples_to_read_1_nov_2019.csv --studyid 5679 --runtag 5679 -resume
-
-echo nextflow started locally with nohup
