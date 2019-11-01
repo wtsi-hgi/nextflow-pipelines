c59df8d7233d59d702a617378cee996c3c1e55fb
gn5 <gn5@sanger.ac.uk>
2019-11-01 14:02:28 +0000

    auto-git

--- bin/rna_seq/nextflow.sh
+++ bin/rna_seq/nextflow.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 echo starting nextflow
 
-# export NXF_VER=19.09.0-edge
-#export NXF_OPTS="-Xms8G -Xmx8G -Dnxf.pool.type=sync -Dnxf.pool.maxThreads=2000"
 export NXF_OPTS="-Xms8G -Xmx8G -Dnxf.pool.maxThreads=2000"
-# export NXF_JAVA_HOME="/lustre/scratch115/realdata/mdt2/projects/bioaid/mercury_gn5/bioaid/env"
-# export JAVA_HOME="/lustre/scratch115/realdata/mdt2/projects/bioaid/mercury_gn5/bioaid/env"
-# export JAVA_CMD="/lustre/scratch115/realdata/mdt2/projects/bioaid/mercury_gn5/bioaid/env/bin/java"
-nextflow run ./pipelines/rna_seq.nf -profile farm4_singularity_gn5 -resume 
-# nextflow run main.nf -profile farm4_singularity_gn5 -resume 
+nextflow run ./pipelines/rna_seq.nf -c ./nextflow-pipelines/nextflow.config -profile farm4_singularity_gn5 -resume 
