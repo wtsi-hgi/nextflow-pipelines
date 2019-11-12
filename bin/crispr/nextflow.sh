3c35fec77101d485263a7989c788d48a4e3ff016
gn5 <gn5@sanger.ac.uk>
2019-11-12 10:49:40 +0000

    auto-git

--- bin/crispr/nextflow.sh
+++ bin/crispr/nextflow.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo starting nextflow
 
-export NXF_OPTS="-Xms8G -Xmx8G -Dnxf.pool.maxThreads=2000"
+export NXF_OPTS="-Xms2G -Xmx2G -Dnxf.pool.maxThreads=2000"
 nextflow run ./nextflow-pipelines/pipelines/crispr.nf -c ./nextflow-pipelines/nextflow.config -profile farm4_singularity_gn5 -resume 
