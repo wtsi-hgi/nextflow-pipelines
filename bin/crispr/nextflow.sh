fd53b87fe470614b00812a90571614322a82a271
gn5 <gn5@sanger.ac.uk>
2019-11-20 17:12:20 +0000

    auto-git

--- bin/crispr/nextflow.sh
+++ bin/crispr/nextflow.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo starting nextflow
 
-export _JAVA_OPTIONS='-XX:StartFlightRecording=filename=JavaFlightRecording.jfr,dumponexit=true,maxage=5d,settings=default'
+export _JAVA_OPTIONS='-XX:StartFlightRecording=filename=JavaFlightRecording.jfr,disk=true,dumponexit=true,maxage=5d,settings=default'
 export NXF_OPTS='-Xms2G -Xmx2G -Dnxf.pool.maxThreads=2000'
 nextflow run ./nextflow-pipelines/pipelines/crispr.nf -c ./nextflow-pipelines/nextflow.config -profile farm4_singularity_gn5 -resume 
