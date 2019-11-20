255975b110f35d4fb2aa3bdb1eb162deba60eb16
gn5 <gn5@sanger.ac.uk>
2019-11-20 14:52:50 +0000

    auto-git

--- bin/crispr/nextflow.sh
+++ bin/crispr/nextflow.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo starting nextflow
 
-export NXF_OPTS='-Xms2G -Xmx2G -Dnxf.pool.maxThreads=2000 -XX:+FlightRecorder  -XX:StartFlightRecording=duration=60s,filename=myrecording.jfr'
+export NXF_OPTS='-Xms2G -Xmx2G -Dnxf.pool.maxThreads=2000 -XX:+FlightRecorder -XX:StartFlightRecording=duration=60s,filename=myrecording.jfr'
 nextflow run ./nextflow-pipelines/pipelines/crispr.nf -c ./nextflow-pipelines/nextflow.config -profile farm4_singularity_gn5 -resume 
