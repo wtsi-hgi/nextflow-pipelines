4ff79f3dff39e04f08a43b40d8b4185d066d8e47
gn5 <gn5@sanger.ac.uk>
2019-12-06 14:16:48 +0000

    auto-git

--- bin/crispr/collate_counts.py
+++ bin/crispr/collate_counts.py
@@ -21,15 +21,15 @@ count_matrix.to_csv(lib_counts + '.count_matrix.txt', sep='\t')
 
 # matrix with gene symbols added
 for index, row in fofn_countsfiles.iterrows():
-    print(row['count_file'].replace('.counts.txt','.genes.counts.txt'))
+    print(row['count_file'].replace('.counts.txt','.subpools.counts.txt'))
     if index == 0:
-        concatenated = pd.read_csv(row['count_file'].replace('.counts.txt','.genes.counts.txt'), sep='\t')
+        concatenated = pd.read_csv(row['count_file'].replace('.counts.txt','.subpools.counts.txt'), sep='\t')
         concatenated['samplename']=row['samplename']
     else:
-        to_concatenate = pd.read_csv(row['count_file'].replace('.counts.txt','.genes.counts.txt'), sep='\t')
+        to_concatenate = pd.read_csv(row['count_file'].replace('.counts.txt','.subpools.counts.txt'), sep='\t')
         to_concatenate['samplename']=row['samplename']
         concatenated = pd.concat([concatenated, to_concatenate], ignore_index=True)
     print(concatenated.head())
         
 count_matrix = concatenated.pivot_table(index=['Guide Sequence','Gene'], columns='samplename', values='Count').reset_index()       
-count_matrix.to_csv(lib_counts + '.genes.count_matrix.txt', sep='\t', index = False)
+count_matrix.to_csv(lib_counts + '.subpools.count_matrix.txt', sep='\t', index = False)
