335d2994c6a7a008e7383fdc0f350cce1d8b87c6
gn5 <gn5@sanger.ac.uk>
2019-12-10 12:47:52 +0000

    auto-git

--- bin/sv-pipeline/nextflow.sh
+++ bin/sv-pipeline/nextflow.sh
@@ -2,4 +2,4 @@
 echo starting nextflow
 
 export NXF_OPTS='-Xms4G -Xmx4G -Dnxf.pool.maxThreads=2000'
-nextflow run ./nextflow-pipelines/pipelines/sv-pipeline.nf -c ./nextflow_wr_openstack.config -resume 
+nextflow run ./nextflow-pipelines/pipelines/sv-pipeline.nf -c ./nextflow_wr/nextflow_wr_openstack.config -resume 
