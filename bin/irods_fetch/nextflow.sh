5e599fc3eaad76a12705ec72ad4ed2da15cde947
gn5 <gn5@sanger.ac.uk>
2019-12-18 15:17:25 +0000

    auto-git

--- bin/irods_fetch/nextflow.sh
+++ bin/irods_fetch/nextflow.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo starting nextflow
 
-export NXF_OPTS="-Xms8G -Xmx8G -Dnxf.pool.maxThreads=2000"
-nextflow run ./nextflow-pipelines/pipelines/irods_fetch.nf -c ./nextflow-pipelines/nextflow.config -profile farm4_singularity_gn5 --samplefile ./data/single_file_samples_to_read_1_nov_2019.csv --studyid 5679 --runtag 5679 -resume
+export NXF_OPTS="-Xms3G -Xmx3G -Dnxf.pool.maxThreads=2000"
+nextflow run ./nextflow-pipelines/pipelines/irods_fetch.nf -c ./nextflow-pipelines/nextflow.config -profile farm4_singularity_gn5 -resume
 
