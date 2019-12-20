b6eff394673269826e93b04bb403280e2e544629
gn5 <gn5@sanger.ac.uk>
2019-12-20 15:52:50 +0000

    auto-git

--- bin/rna_seq/deseq2.R
+++ bin/rna_seq/deseq2.R
@@ -1,28 +1,59 @@
 #!/usr/bin/env Rscript
 args = commandArgs(trailingOnly=TRUE)
 
-if (length(args)==0) {stop("arguments needed", call.=FALSE)}
+if (length(args)==0) {
+  stop("At least one argument must be supplied", call.=FALSE)}
 
+library(AnnotationHub)
 library(tximport)
 library(magrittr)
 library(readr)
+queries = query(AnnotationHub(), c(args[1], "Homo sapiens"))
+edb = queries[[1]]
+edb
+Tx <- transcripts(edb, return.type="DataFrame")
+head(Tx)
+tx2gene <- Tx[,c(9,7)]
+## tx_id         gene_id
+##           <character>     <character>
+##1      ENST00000387314 ENSG00000210049
+head(tx2gene)
 
-experiment_df = read_tsv(args[0])
-txi_gene_counts = read.csv(args[1])
-txi_transcript_counts = read.csv(args[2])
-txi_tpm_counts = read.csv(args[3]) # tx.salmon.scale <- tximport(files, type = "salmon", tx2gene = tx2gene, countsFromAbundance = "lengthScaledTPM"
+files_df = read.csv(args[2], header = FALSE)
+files <- file.path(files_df[,1])
+
+names(files) <- as.character(files)
+all(file.exists(files))
+
+# gene-level summarization 
+txi <- tximport(files, type = "salmon", tx2gene = tx2gene)
+head(txi$counts)
+write.csv(txi$counts, "txi_gene_counts.csv")
+
+# We can avoid gene-level summarization by setting txOut=TRUE,
+# giving the original transcript level estimates as a list of matrices.
+txi.tx <- tximport(files, type = "salmon", txOut = TRUE)
+write.csv(txi.tx$counts, "txi_transcript_counts.csv")
+                                        #
+# count table from TPM 
+# accounts for transcript length changes across samples and library size differences.
+tx.salmon.scale <- tximport(files, type = "salmon", tx2gene = tx2gene, 
+                            countsFromAbundance = "lengthScaledTPM")
 write.csv(tx.salmon.scale$counts, "txi_lengthScaledTPM_gene_counts.csv")
 
-#######
+#################
+
+
+experiment_df = read_tsv(args[3]) %>% as.data.frame
+#print(experiment_df)
+row.names(experiment_df) = experiment_df$sample
+experiment_df %<>% select(g,r,a) %>% mutate_if(is.character, as.factor)
+experiment_df %<>% select(g,r,a) %>% mutate_if(is.numeric, as.factor)
+
+
 library("DESeq2")
-dds <- DESeqDataSetFromMatrix(countData = txi_gene_counts,
-                              colData = experiment_df,
-                              design = ~ experiment_df$model_formula[0])
-
-design(ddsMF) <- formula(~ type + condition)
-ddsMF <- DESeq(ddsMF)
-Again, we access the results using the results function.
-
-
+ddsTxi <- DESeqDataSetFromTximport(txi,
+                                   colData = experiment_df,
+                                   design = ~ 1)
 
 save.image("./deseq2.rdata")
