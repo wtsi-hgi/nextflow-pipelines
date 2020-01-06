98b478f926ca5c044c9328224c58a75e8cf6ef5b
gn5 <gn5@sanger.ac.uk>
2020-01-06 14:17:42 +0000

    auto-git

--- bin/rna_seq/nextflow.sh
+++ bin/rna_seq/nextflow.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 echo starting nextflow
 
+export NXF_VER=19.12.0-edge
 export NXF_OPTS="-Xms8G -Xmx8G -Dnxf.pool.maxThreads=2000"
 nextflow run ./nextflow-pipelines/pipelines/rna_seq.nf -c ./nextflow-pipelines/nextflow.config -profile farm4_singularity_gn5 -resume 
