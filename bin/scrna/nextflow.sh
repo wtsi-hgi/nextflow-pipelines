07a78036a357e961ff2f9b3857bfe194866a5efe
gn5 <gn5@sanger.ac.uk>
2020-01-06 15:33:52 +0000

    auto-git

--- bin/scrna/nextflow.sh
+++ bin/scrna/nextflow.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 echo starting nextflow
 
-export NXF_OPTS='-Xms2G -Xmx2G -Dnxf.pool.maxThreads=2000'
+export NXF_VER=19.12.0-edge
+export NXF_OPTS='-Xms4G -Xmx4G -Dnxf.pool.maxThreads=2000'
 nextflow run ./nextflow-pipelines/pipelines/scrna.nf -c ./nextflow-pipelines/nextflow.config -profile farm4_singularity_gn5 -resume 
