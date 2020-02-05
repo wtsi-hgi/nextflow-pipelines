abed2c8ad839b3ce4513802701361edd337f45fe
gn5 <gn5@sanger.ac.uk>
2020-02-05 15:42:01 +0000

    auto-git

--- bin/variant_calling/nextflow.sh
+++ bin/variant_calling/nextflow.sh
@@ -2,6 +2,6 @@
 echo starting nextflow
 
 export NXF_VER=19.12.0-edge
-# export NXF_OPTS="-Xms6G -Xmx6G -Dnxf.pool.type=sync -Dnxf.pool.maxThreads=4000"
-export NXF_OPTS="-Xms2G -Xmx2G"
+export NXF_OPTS="-Xms6G -Xmx6G -Dnxf.pool.type=sync -Dnxf.pool.maxThreads=4000"
+#export NXF_OPTS="-Xms2G -Xmx2G"
 nextflow run ./nextflow-pipelines/pipelines/variant_calling.nf -c ./nextflow-pipelines/nextflow.config -profile farm4_singularity_gn5 -resume 
