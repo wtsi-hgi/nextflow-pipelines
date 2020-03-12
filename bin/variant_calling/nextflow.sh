425af4b03b1cf4c2ce9592c3038c3527b93f993e
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-03-12 14:02:11 +0000

    added more memory

--- bin/variant_calling/nextflow.sh
+++ bin/variant_calling/nextflow.sh
@@ -2,5 +2,5 @@
 echo starting nextflow
 
 #export NXF_OPTS="-Xms7G -Xmx7G -Dnxf.pool.type=sync -Dnxf.pool.maxThreads=4000"
-export NXF_OPTS="-Xms6G -Xmx6G"
+export NXF_OPTS="-Xms750G -Xmx750G"
 nextflow run ./nextflow-pipelines/pipelines/as_vqsr.nf -c ./nextflow-pipelines/nextflow.config -profile farm4_singularity_gn5 -resume 
