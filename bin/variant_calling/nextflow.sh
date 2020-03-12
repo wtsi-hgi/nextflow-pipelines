7196ae27941c369c347be282ae0b9b4a2a1f8e22
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-03-12 13:55:27 +0000

    changed nextflow.sh to point to allele specific pipeline

--- bin/variant_calling/nextflow.sh
+++ bin/variant_calling/nextflow.sh
@@ -3,4 +3,4 @@ echo starting nextflow
 
 #export NXF_OPTS="-Xms7G -Xmx7G -Dnxf.pool.type=sync -Dnxf.pool.maxThreads=4000"
 export NXF_OPTS="-Xms6G -Xmx6G"
-nextflow run ./nextflow-pipelines/pipelines/variant_calling.nf -c ./nextflow-pipelines/nextflow.config -profile farm4_singularity_gn5 -resume 
+nextflow run ./nextflow-pipelines/pipelines/as_vqsr.nf -c ./nextflow-pipelines/nextflow.config -profile farm4_singularity_gn5 -resume 
