c0d0355a9a7cc3ec6c81c9f3e6dafe1c2998d40e
gn5 <gn5@sanger.ac.uk>
2021-01-07 11:49:14 +0000

    auto-git

--- bin/crispr/nextflow.sh
+++ bin/crispr/nextflow.sh
@@ -3,4 +3,4 @@ echo starting nextflow
 
 #export NXF_OPTS="-Xms7G -Xmx7G -Dnxf.pool.type=sync -Dnxf.pool.maxThreads=4000"
 export NXF_OPTS="-Xms3G -Xmx3G"
-nextflow run ./nextflow-pipelines/pipelines/crispr.nf -c ./nextflow-pipelines/nextflow.config -profile farm4_singularity_gn5 -resume 
+nextflow run ./nextflow-pipelines/pipelines/crispr.nf -c ./nextflow-pipelines/nextflow.config -profile farm5_lsf_singularity -resume 
